4d209f2e47be6e47eae15ec79fe2f9b4c56667ac
baptistegosselet <baptiste.gosselet.outlook.fr>
2024-07-02 15:17:49 +0200

    🔧 Configuration minimale

---
+++ src/main/java/com/bgosselet/blankApp/BlankAppApplication.java
@@ -0,0 +1,14 @@
+package com.bgosselet.blankApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BlankAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlankAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bgosselet/blankApp/BlankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgosselet.blankApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
